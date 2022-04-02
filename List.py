9b2bfd02fa29219ba37e58fadf2608302ceb51f3
Tregz <tregzmusic@gmail.com>
2022-04-02 20:26:33 +0300

    add list

---
+++ List.py
@@ -0,0 +1,17 @@
+#write list
+lst = [1, 2, 3, 4, 5, 6, 7]
+
+#add 8 in end
+lst.append("8")
+
+#add in index 4 = 18
+lst[4] = 18
+
+#add list in end
+lst.append(["name", "file"])
+
+#add turple
+#add turple
+lst[2] = (100, 101, 102)
+
+print(lst)
